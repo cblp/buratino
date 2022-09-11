1c90b0d18fbee78dc7ee22b572b5553b4ba0da23
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-09-11 18:22:17 +0200

    Add save rates to db

--- fetcher.py
+++ fetcher.py
@@ -1,3 +1,4 @@
+import sqlite3
 from decimal import Decimal
 import requests
 import xml.etree.ElementTree as ET
@@ -5,14 +6,18 @@ import xml.etree.ElementTree as ET
 url = 'http://www.cbr.ru/scripts/XML_daily.asp'
 response = requests.get(url)
 response.raise_for_status()
-
 root = ET.fromstring(response.text)
-r = [
-    (
-        valute.find("CharCode").text,
+db = sqlite3.connect('rates.db')
+for valute in root.findall("Valute"):
+    currency = valute.find("CharCode").text
+    rate = (
         Decimal(valute.find("Value").text.replace(",", "."))
-            / Decimal(valute.find("Nominal").text)
+        / Decimal(valute.find("Nominal").text)
     )
-    for valute in root.findall("Valute")
-]
-print(r)
+    r = db.execute(f'''
+        INSERT INTO rates
+            (currency, base, rate) VALUES ('{currency}', 'RUB', {rate})
+            ON CONFLICT DO UPDATE SET rate = {rate};
+    ''')
+    print(currency, rate, r)
+db.commit()
