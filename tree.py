7050a14f179bca49b59cb04e228018a59ae214e9
Maxim <sadritdinovmax2@gmail.com>
2022-10-09 01:26:54 +0300

    tree

--- tree.py
+++ tree.py
@@ -1,16 +1,19 @@
-SPACE = ' '
-STRAR = '*'
 
-if __name__ == "__main__":
-    rows = 1
-    spaces = rows-1
-    stars = 2
+def tree(x):
+    SPACE = ' '
+    STRAR = '*'
 
-    for i in range(rows):
-        print(
-            (SPACE*spaces) +
-            (STRAR*stars) +
-            (SPACE*spaces)
-        )
-        stars += 2
-        spaces -= 1
+    if __name__ == "__main__":
+        rows = x
+        spaces = rows - 1
+        stars = 1
+
+        for i in range(rows):
+            print(
+                (SPACE * spaces) +
+                (STRAR * stars) +
+                (SPACE * spaces)
+            )
+            stars += 2
+            spaces -= 1
+print (tree(int(input())))
